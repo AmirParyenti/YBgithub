dc7fa9cd5d10e2d2509b1815447d6607429dd266
Amir Paryenti <167919910+AmirParyenti@users.noreply.github.com>
2024-05-18 21:50:46 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 console.log("hey");
+
+function checkEqual(a, b) {
+  if (a == b) { // Noncompliant: using non-strict equality '=='
+    return "Equal";
+  } else {
+    return "Not equal";
+  }
+}
+
+console.log(checkEqual(0, false)); // Output: "Equal"
