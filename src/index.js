6988522fec5381341ca50d962b013ffbfddbc62f
Amir Paryenti <167919910+AmirParyenti@users.noreply.github.com>
2024-05-18 21:56:07 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
 console.log("hey");
 
-function checkEqual(a, b) {
-  if (a == b) { // Noncompliant: using non-strict equality '=='
-    return "Equal";
-  } else {
-    return "Not equal";
-  }
-}
-
-console.log(checkEqual(0, false)); // Output: "Equal"
+const pi = 3.14;
+pi = 3.14159; // Noncompliant: TypeError: invalid assignment to const 'pi'
